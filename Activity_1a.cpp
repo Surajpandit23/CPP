00f69012f7212f398e4fe689d05dbf42abee5cfc
Suraj Pandit <106903703+Surajpandit23@users.noreply.github.com>
2023-05-19 23:03:21 +0530

    Add files via upload

---
+++ Activity_1a.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class student{         //student class
+  private:
+  string id,branch,sem;
+  public:
+  student(string id,string branch, string sem){
+
+  }
+};
+                       //course class
+class course{
+  pivate:
+  string c_name,sem;
+  int c_id;
+
+public:
+    course(string c_name,string sem, int c_id){
+
+    }
+};
+
+class faculty{
+  private:
+  string id,branch,email;
+  public:
+  faculty(string id ,strung branch,string email){
+
+  }
+};
+
+
